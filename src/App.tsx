6deee2d4a2dcfa46e8131dae97a16347f61be50f
rherault <romain@rherault.fr>
2023-01-20 15:00:09 +0100

    dep: adding base dependencies for 3d projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { useControls } from 'leva'
+import { Perf } from 'r3f-perf'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { togglePerf, color } = useControls({
+    togglePerf: { value: true, label: 'Toggle Perf' },
+    color: { value: 'mediumpurple', label: 'Color' },
+  })
 
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </div>
+    <Canvas>
+      { togglePerf && <Perf position="top-left"/> }
+
+      <OrbitControls makeDefault />
+
+      <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+      <ambientLight intensity={ 0.5 } />
+
+      <mesh scale={ 1.5 }>
+        <boxGeometry />
+        <meshStandardMaterial color={ color } />
+      </mesh>
+    </Canvas>
   )
 }
 
