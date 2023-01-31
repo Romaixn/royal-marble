b71baac46777328fd35d3f86b54d11a9f2782a8d
Romain <romain@rherault.fr>
2023-01-31 12:11:50 +0100

    clean: cleanup unused models

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,38 @@
+import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { useGLTF, OrbitControls } from '@react-three/drei'
-import { Debug, RigidBody, Physics } from '@react-three/rapier'
+import { Loader, Sky, Stage, OrbitControls } from '@react-three/drei'
+import { Debug, Physics } from '@react-three/rapier'
 import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
 
 function App() {
-  const start = useGLTF('/models/end_square.glb')
-  const ramp = useGLTF('/models/ramp_long_C.glb')
-  const hole = useGLTF('/models/end_hole_square.glb')
-
   const { debug, perf } = useControls({
     perf: { value: true, label: 'Performance' },
-    debug: { value: false, label: 'Debug' },
+    debug: { value: true, label: 'Debug' },
   })
 
   return (
-    <Canvas>
-      { perf && <Perf position="top-left"/> }
+    <>
+      <Canvas>
+        <Suspense fallback={null}>
+          { perf && <Perf position="top-left"/> }
 
-      <OrbitControls makeDefault />
+          <Stage
+            intensity={0.5}
+            preset="rembrandt"
+            adjustCamera={1.75}
+            environment="city">
+            <Physics>
+              { debug && <Debug /> }
+            </Physics>
+          </Stage>
 
-      <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-      <ambientLight intensity={ 0.5 } />
-
-      <Physics>
-        { debug && <Debug /> }
-        <RigidBody colliders="ball" restitution={0.5} friction={1} linearDamping={0.5} angularDamping={0.5}>
-          <mesh position-y={ 5 } position-z={ -3.5 } castShadow receiveShadow>
-            <sphereGeometry args={ [ 0.4, 16, 16 ] } />
-            <meshStandardMaterial color="white" />
-          </mesh>
-        </RigidBody>
-
-        <RigidBody type="fixed" colliders="trimesh" position-y={ 2 } position-z={ -3 }>
-          <primitive object={ start.scene } scale={ 2 } receiveShadow />
-        </RigidBody>
-
-        <RigidBody type="fixed" colliders="trimesh" position-y={ 0 }>
-          <primitive object={ ramp.scene } scale={ 2 } receiveShadow />
-        </RigidBody>
-
-        <RigidBody type="fixed" colliders="trimesh" position-z={ 3 }>
-          <primitive object={ hole.scene } scale={ 2 } rotation-y={ Math.PI } receiveShadow />
-        </RigidBody>
-
-        <RigidBody type="fixed" restitution={ 0 } friction={ 0.7 }>
-            <mesh receiveShadow>
-              <boxGeometry args={ [ 10, 0.5, 10 ] } />
-              <meshStandardMaterial color="greenyellow" />
-            </mesh>
-        </RigidBody>
-      </Physics>
-    </Canvas>
+          <OrbitControls makeDefault />
+          <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   )
 }
 
