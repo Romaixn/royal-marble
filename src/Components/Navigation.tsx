fa4125d2c7a7a7ba6ec8cd701fbf40e16fe5cc88
rherault <romain@rherault.dev>
2023-02-22 17:27:18 +0100

    fix: animation on button

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -6,15 +6,22 @@ function Navigation(): JSX.Element {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <motion.div
-      layout
-      data-isOpen={isOpen}
-      initial={{ borderRadius: 50 }}
-      className="data-[isOpen=true]:w-1/2 data-[isOpen=true]:h-96 data-[isOpen=false]:cursor-pointer inline-flex items-center rounded-full border border-transparent bg-slate-600 p-1 text-white shadow-sm hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2 pointer-events-auto"
-      onClick={() => setIsOpen(!isOpen)}
-    >
-      <CubeIcon className="h-10 w-10" />
-    </motion.div>
+    <div className="relative">
+      <motion.div
+        layout
+        data-isOpen={isOpen}
+        className="data-[isOpen=true]:w-max data-[isOpen=true]:h-fit data-[isOpen=false]:cursor-pointer absolute -bottom-6 left-0 inline-flex items-center rounded-full border border-transparent bg-slate-600 p-1 text-white shadow-sm hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2 pointer-events-auto"
+        onClick={() => setIsOpen(!isOpen)}
+        >
+          { !isOpen && <CubeIcon className="h-10 w-10" /> }
+          { isOpen &&
+            <div className="p-4">
+              <p>Lorem ipsum dolor sit amet</p>
+              <p>Lorem ipsum dolor sit amet</p>
+            </div>
+          }
+        </motion.div>
+      </div>
   )
 }
 
